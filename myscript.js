d1e7238bfc4c1e46f7afa0f4c6a14e9e94defba1
FajarFatha <fajarfathar@gmail.com>
2021-05-13 01:42:35 +0700

    membuat fungsi

--- myscript.js
+++ myscript.js
@@ -1,1 +1,273 @@
+function batik1(){
+    document.getElementById("batik1").style.display="block"
+    document.getElementById("nbbatik1").style.display="none"
+}
+function tutupbatik1(){
+    document.getElementById("batik1").style.display="none"
+    document.getElementById("nbbatik1").style.display="block"
+}
 
+function batik2(){
+    document.getElementById("batik2").style.display="block"
+    document.getElementById("nbbatik2").style.display="none"
+}
+function tutupbatik2(){
+    document.getElementById("batik2").style.display="none"
+    document.getElementById("nbbatik2").style.display="block"
+}
+
+function batik3(){
+    document.getElementById("batik3").style.display="block"
+    document.getElementById("nbbatik3").style.display="none"
+}
+function tutupbatik3(){
+    document.getElementById("batik3").style.display="none"
+    document.getElementById("nbbatik3").style.display="block"
+}
+
+function batik4(){
+    document.getElementById("batik4").style.display="block"
+    document.getElementById("nbbatik4").style.display="none"
+}
+function tutupbatik4(){
+    document.getElementById("batik4").style.display="none"
+    document.getElementById("nbbatik4").style.display="block"
+}
+
+function batik5(){
+    document.getElementById("batik1").style.display="block"
+    document.getElementById("nbbatik1").style.display="none"
+}
+function tutupbatik5(){
+    document.getElementById("batik5").style.display="none"
+    document.getElementById("nbbatik5").style.display="block"
+}
+
+function batik6(){
+    document.getElementById("batik6").style.display="block"
+    document.getElementById("nbbatik6").style.display="none"
+}
+function tutupbatik6(){
+    document.getElementById("batik6").style.display="none"
+    document.getElementById("nbbatik6").style.display="block"
+}
+
+
+function lukisan1(){
+    document.getElementById("lukisan1").style.display="block"
+    document.getElementById("nblukisan1").style.display="none"
+}
+function tutuplukisan1(){
+    document.getElementById("lukisan1").style.display="none"
+    document.getElementById("nblukisan1").style.display="block"
+}
+
+function lukisan2(){
+    document.getElementById("lukisan2").style.display="block"
+    document.getElementById("nblukisan2").style.display="none"
+}
+function tutuplukisan2(){
+    document.getElementById("lukisan2").style.display="none"
+    document.getElementById("nblukisan2").style.display="block"
+}
+
+function lukisan3(){
+    document.getElementById("lukisan3").style.display="block"
+    document.getElementById("nblukisan3").style.display="none"
+}
+function tutuplukisan3(){
+    document.getElementById("lukisan3").style.display="none"
+    document.getElementById("nblukisan3").style.display="block"
+}
+
+function lukisan4(){
+    document.getElementById("lukisan4").style.display="block"
+    document.getElementById("nblukisan4").style.display="none"
+}
+function tutuplukisan4(){
+    document.getElementById("lukisan4").style.display="none"
+    document.getElementById("nblukisan4").style.display="block"
+}
+
+function lukisan5(){
+    document.getElementById("lukisan5").style.display="block"
+    document.getElementById("nblukisan5").style.display="none"
+}
+function tutuplukisan5(){
+    document.getElementById("lukisan5").style.display="none"
+    document.getElementById("nblukisan5").style.display="block"
+}
+
+function lukisan6(){
+    document.getElementById("lukisan6").style.display="block"
+    document.getElementById("nblukisan6").style.display="none"
+}
+function tutuplukisan6(){
+    document.getElementById("lukisan6").style.display="none"
+    document.getElementById("nblukisan6").style.display="block"
+}
+
+
+function patung1(){
+    document.getElementById("patung1").style.display="block"
+    document.getElementById("nbpatung1").style.display="none"
+}
+function tutuppatung1(){
+    document.getElementById("patung1").style.display="none"
+    document.getElementById("nbpatung1").style.display="block"
+}
+
+function patung2(){
+    document.getElementById("patung2").style.display="block"
+    document.getElementById("nbpatung2").style.display="none"
+}
+function tutuppatung2(){
+    document.getElementById("patung2").style.display="none"
+    document.getElementById("nbpatung2").style.display="block"
+}
+
+function patung3(){
+    document.getElementById("patung3").style.display="block"
+    document.getElementById("nbpatung3").style.display="none"
+}
+function tutuppatung3(){
+    document.getElementById("patung3").style.display="none"
+    document.getElementById("nbpatung3").style.display="block"
+}
+
+function patung4(){
+    document.getElementById("patung4").style.display="block"
+    document.getElementById("nbpatung4").style.display="none"
+}
+function tutuppatung4(){
+    document.getElementById("patung4").style.display="none"
+    document.getElementById("nbpatung4").style.display="block"
+}
+
+function patung5(){
+    document.getElementById("patung5").style.display="block"
+    document.getElementById("nbpatung5").style.display="none"
+}
+function tutuppatung5(){
+    document.getElementById("patung5").style.display="none"
+    document.getElementById("nbpatung5").style.display="block"
+}
+
+function patung6(){
+    document.getElementById("patung6").style.display="block"
+    document.getElementById("nbpatung6").style.display="none"
+}
+function tutuppatung6(){
+    document.getElementById("patung6").style.display="none"
+    document.getElementById("nbpatung6").style.display="block"
+}
+
+
+function musik1(){
+    document.getElementById("musik1").style.display="block"
+    document.getElementById("nbmusik1").style.display="none"
+}
+function tutupmusik1(){
+    document.getElementById("musik1").style.display="none"
+    document.getElementById("nbmusik1").style.display="block"
+}
+
+function musik2(){
+    document.getElementById("musik2").style.display="block"
+    document.getElementById("nbmusik2").style.display="none"
+}
+function tutupmusik2(){
+    document.getElementById("musik2").style.display="none"
+    document.getElementById("nbmusik2").style.display="block"
+}
+
+function musik3(){
+    document.getElementById("musik3").style.display="block"
+    document.getElementById("nbmusik3").style.display="none"
+}
+function tutupmusik3(){
+    document.getElementById("musik3").style.display="none"
+    document.getElementById("nbmusik3").style.display="block"
+}
+
+function musik4(){
+    document.getElementById("musik4").style.display="block"
+    document.getElementById("nbmusik4").style.display="none"
+}
+function tutupmusik4(){
+    document.getElementById("musik4").style.display="none"
+    document.getElementById("nbmusik4").style.display="block"
+}
+
+function musik5(){
+    document.getElementById("musik5").style.display="block"
+    document.getElementById("nbmusik5").style.display="none"
+}
+function tutupmusik5(){
+    document.getElementById("musik5").style.display="none"
+    document.getElementById("nbmusik5").style.display="block"
+}
+
+function musik6(){
+    document.getElementById("musik6").style.display="block"
+    document.getElementById("nbmusik6").style.display="none"
+}
+function tutupmusik6(){
+    document.getElementById("musik6").style.display="none"
+    document.getElementById("nbmusik6").style.display="block"
+}
+
+
+function tari1(){
+    document.getElementById("tari1").style.display="block"
+    document.getElementById("nbtari1").style.display="none"
+}
+function tutuptari1(){
+    document.getElementById("tari1").style.display="none"
+    document.getElementById("nbtari1").style.display="block"
+}
+
+function tari2(){
+    document.getElementById("tari2").style.display="block"
+    document.getElementById("nbtari2").style.display="none"
+}
+function tutuptari2(){
+    document.getElementById("tari2").style.display="none"
+    document.getElementById("nbtari2").style.display="block"
+}
+
+function tari3(){
+    document.getElementById("tari3").style.display="block"
+    document.getElementById("nbtari3").style.display="none"
+}
+function tutuptari3(){
+    document.getElementById("tari3").style.display="none"
+    document.getElementById("nbtari3").style.display="block"
+}
+
+function tari4(){
+    document.getElementById("tari4").style.display="block"
+    document.getElementById("nbtari4").style.display="none"
+}
+function tutuptari4(){
+    document.getElementById("tari4").style.display="none"
+    document.getElementById("nbtari4").style.display="block"
+}
+
+function tari5(){
+    document.getElementById("tari5").style.display="block"
+    document.getElementById("nbtari5").style.display="none"
+}
+function tutuptari5(){
+    document.getElementById("tari5").style.display="none"
+    document.getElementById("nbtari5").style.display="block"
+}
+
+function tari6(){
+    document.getElementById("tari6").style.display="block"
+    document.getElementById("nbtari6").style.display="none"
+}
+function tutuptari6(){
+    document.getElementById("tari6").style.display="none"
+    document.getElementById("nbtari6").style.display="block"
+}
